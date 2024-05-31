d460f0428abe603dc975001eedebf9394f08ecb7
IoanaDodi <ioanadodita1998@gmail.com>
2024-05-31 19:36:45 +0300

    tema 1 finalizata

---
+++ tema1/index.js
@@ -0,0 +1,10 @@
+// Scrie o functie care returneaza patratul unui numar
+//Funcția findSquare(num) returnează pătratul numărului primit ca date de intrare (este transmis ca argument în apelul funcției).
+
+const findSquare = (n) => n ** 2;
+
+console.log(findSquare(2));
+console.log(findSquare(10));
+console.log(findSquare(0));
+console.log(findSquare(100));
+console.log(findSquare(-25));
